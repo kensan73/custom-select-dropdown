c35cf0c4d1619971d686ac5473d0a3b30a157cd2
ken ore <ken_ore@hotmail.com>
2025-03-08 16:24:50 -0600

    Independent expand / collapse working.

--- src/demo.js
+++ src/demo.js
@@ -1,11 +1,19 @@
-import * as React from 'react';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
+import * as React from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import StarBorder from "@mui/icons-material/StarBorder";
+import Collapse from "@mui/material/Collapse";
+import List from "@mui/material/List";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -21,62 +29,62 @@ const MenuProps = {
 const variants = [
   {
     id: 3,
-    name: 'Voucher',
-    slug: 'voucher',
-    type: 'Main',
-    locale: 'en',
-    created_at: '2021-11-15T08:27:23.000Z',
-    updated_at: '2021-11-15T08:27:23.000Z',
+    name: "Voucher",
+    slug: "voucher",
+    type: "Main",
+    locale: "en",
+    created_at: "2021-11-15T08:27:23.000Z",
+    updated_at: "2021-11-15T08:27:23.000Z",
     cover: null,
   },
   {
     id: 1,
-    name: 'Top Up',
-    slug: 'top-up',
-    type: 'Main',
-    locale: 'en',
-    created_at: '2021-11-15T08:26:44.000Z',
-    updated_at: '2021-11-15T08:26:44.000Z',
+    name: "Top Up",
+    slug: "top-up",
+    type: "Main",
+    locale: "en",
+    created_at: "2021-11-15T08:26:44.000Z",
+    updated_at: "2021-11-15T08:26:44.000Z",
     cover: null,
   },
   {
     id: 2,
-    name: 'Game Key',
-    slug: 'game-key',
-    type: 'Main',
-    locale: 'en',
-    created_at: '2021-11-15T08:27:03.000Z',
-    updated_at: '2021-11-15T08:27:03.000Z',
+    name: "Game Key",
+    slug: "game-key",
+    type: "Main",
+    locale: "en",
+    created_at: "2021-11-15T08:27:03.000Z",
+    updated_at: "2021-11-15T08:27:03.000Z",
     cover: null,
   },
   {
     id: 12,
-    name: 'Other',
-    slug: 'other',
-    type: 'SubMain',
-    locale: 'en',
-    created_at: '2021-11-15T08:30:50.000Z',
-    updated_at: '2021-11-15T08:30:50.000Z',
+    name: "Other",
+    slug: "other",
+    type: "SubMain",
+    locale: "en",
+    created_at: "2021-11-15T08:30:50.000Z",
+    updated_at: "2021-11-15T08:30:50.000Z",
     cover: null,
   },
   {
     id: 11,
-    name: 'Nintendo',
-    slug: 'nintendo',
-    type: 'SubMain',
-    locale: 'en',
-    created_at: '2021-11-15T08:30:22.000Z',
-    updated_at: '2021-11-15T08:30:22.000Z',
+    name: "Nintendo",
+    slug: "nintendo",
+    type: "SubMain",
+    locale: "en",
+    created_at: "2021-11-15T08:30:22.000Z",
+    updated_at: "2021-11-15T08:30:22.000Z",
     cover: null,
   },
   {
     id: 10,
-    name: 'Xbox',
-    slug: 'xbox',
-    type: 'SubMain',
-    locale: 'en',
-    created_at: '2021-11-15T08:30:08.000Z',
-    updated_at: '2021-11-15T08:30:08.000Z',
+    name: "Xbox",
+    slug: "xbox",
+    type: "SubMain",
+    locale: "en",
+    created_at: "2021-11-15T08:30:08.000Z",
+    updated_at: "2021-11-15T08:30:08.000Z",
     cover: null,
   },
 ];
@@ -85,25 +93,43 @@ export default function MultipleSelectCheckmarks() {
   const [variantName, setVariantName] = React.useState([
     {
       id: 11,
-      name: 'Nintendo',
-      slug: 'nintendo',
-      type: 'SubMain',
-      locale: 'en',
-      created_at: '2021-11-15T08:30:22.000Z',
-      updated_at: '2021-11-15T08:30:22.000Z',
+      name: "Nintendo",
+      slug: "nintendo",
+      type: "SubMain",
+      locale: "en",
+      created_at: "2021-11-15T08:30:22.000Z",
+      updated_at: "2021-11-15T08:30:22.000Z",
       cover: null,
     },
     {
       id: 10,
-      name: 'Xbox',
-      slug: 'xbox',
-      type: 'SubMain',
-      locale: 'en',
-      created_at: '2021-11-15T08:30:08.000Z',
-      updated_at: '2021-11-15T08:30:08.000Z',
+      name: "Xbox",
+      slug: "xbox",
+      type: "SubMain",
+      locale: "en",
+      created_at: "2021-11-15T08:30:08.000Z",
+      updated_at: "2021-11-15T08:30:08.000Z",
       cover: null,
     },
   ]);
+
+  const [open, setOpen] = React.useState([true, true, true, true, true, true]);
+
+  function isThisOpen(index) {
+    return open[index];
+  }
+
+  function toggleThis(index) {
+    setOpen((prevState) => [
+      ...prevState.slice(0, index),
+      !prevState[index],
+      ...prevState.slice(index + 1),
+    ]);
+  }
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   const handleChange = (event) => {
     const {
@@ -114,8 +140,8 @@ export default function MultipleSelectCheckmarks() {
     );
     setVariantName(
       // On autofill we get a the stringified value.
-      typeof preventDuplicate === 'string'
-        ? preventDuplicate.split(',')
+      typeof preventDuplicate === "string"
+        ? preventDuplicate.split(",")
         : preventDuplicate
     );
   };
@@ -123,21 +149,37 @@ export default function MultipleSelectCheckmarks() {
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+        <InputLabel id='demo-multiple-checkbox-label'>Tag</InputLabel>
         <Select
-          labelId="demo-multiple-checkbox-label"
-          id="demo-multiple-checkbox"
+          labelId='demo-multiple-checkbox-label'
+          id='demo-multiple-checkbox'
           multiple
           value={variantName}
           onChange={handleChange}
-          input={<OutlinedInput label="Tag" />}
-          renderValue={(selected) => selected.map((x) => x.name).join(', ')}
+          input={<OutlinedInput label='Tag' />}
+          renderValue={(selected) => selected.map((x) => x.name).join(", ")}
           MenuProps={MenuProps}
         >
-          {variants.map((variant) => (
+          {variants.map((variant, index) => (
             <MenuItem key={variant.id} value={variant}>
-              <Checkbox checked={variantName.indexOf(variant) > -1} />
-              <ListItemText primary={variant.name} />
+              <ListItemButton onClick={() => toggleThis(index)}>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary='Inbox' />
+                {isThisOpen(index) ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={isThisOpen(index)} timeout='auto' unmountOnExit>
+                <List component='div' disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <Checkbox checked={variantName.indexOf(variant) > -1} />
+                    <ListItemText primary={variant.name} />
+                  </ListItemButton>
+                </List>
+              </Collapse>
             </MenuItem>
           ))}
         </Select>
