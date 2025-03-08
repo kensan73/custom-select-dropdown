b7e7f748e407946078e0baa1f2c1154861e3ed46
ken ore <ken_ore@hotmail.com>
2025-03-08 16:35:11 -0600

    Tweaked style to show more.

--- src/demo.js
+++ src/demo.js
@@ -20,8 +20,8 @@ const ITEM_PADDING_TOP = 8;
 const MenuProps = {
   PaperProps: {
     style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
+      maxHeight: ITEM_HEIGHT * 9 + ITEM_PADDING_TOP,
+      width: 500,
     },
   },
 };
